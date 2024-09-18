a549ed4a188d3af3e999c5009e489baae55c96ab
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-09-18 21:04:12 +0800

    added

--- src/components/pages/Lession1.tsx
+++ src/components/pages/Lession1.tsx
@@ -1,13 +1,22 @@
 import { motion } from "framer-motion";
 
 const parent = {
-  fst: { opacity: 0, scale: 0.9 },
-  lst: { opacity: 1, scale: 1 },
-};
-
-const child = {
-  fst: { opacity: 0, scale: 0.9 },
-  lst: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.5,
+    },
+  },
+  hover: {
+    scale: 3,
+    transition: {
+      duration: 2,
+    },
+  },
+  tab: { scale: 1, rotate: 45 },
 };
 
 const Lesson1 = () => {
@@ -16,32 +25,11 @@ const Lesson1 = () => {
       <motion.div
         className="size-64 bg-indigo-600 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-start"
         variants={parent}
-        initial="fst"
-        animate="lst"
-        transition={{
-          ease: "easeInOut",
-          duration: 2,
-          delayChildren: 0.5,
-          staggerChildren: 0.5,
-        }}
-      >
-        <motion.div
-          className="size-20 bg-cyan-500 rounded-md"
-          variants={child}
-        ></motion.div>
-        <motion.div
-          className="size-20 bg-cyan-500 rounded-md"
-          variants={child}
-        ></motion.div>
-        <motion.div
-          className="size-20 bg-cyan-500 rounded-md"
-          variants={child}
-        ></motion.div>
-        <motion.div
-          className="size-20 bg-cyan-500 rounded-md"
-          variants={child}
-        ></motion.div>
-      </motion.div>
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        whileTap={"tab"}
+      ></motion.div>
     </div>
   );
 };
