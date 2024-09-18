c12123a2e4747f8c0d8d7dc486030c3a7c1daf38
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-09-18 20:50:04 +0800

    added more

--- src/components/pages/Lession1.tsx
+++ src/components/pages/Lession1.tsx
@@ -1,14 +1,47 @@
 import { motion } from "framer-motion";
 
+const parent = {
+  fst: { opacity: 0, scale: 0.9 },
+  lst: { opacity: 1, scale: 1 },
+};
+
+const child = {
+  fst: { opacity: 0, scale: 0.9 },
+  lst: { opacity: 1, scale: 1 },
+};
+
 const Lesson1 = () => {
   return (
     <div>
       <motion.div
-        className="size-64 bg-indigo-600 rounded-lg"
-        animate={{ rotate: 360 }}
-        initial={{ rotate: 0 }}
-        transition={{ duration: 3, type: "spring" }}
-      ></motion.div>
+        className="size-64 bg-indigo-600 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-start"
+        variants={parent}
+        initial="fst"
+        animate="lst"
+        transition={{
+          ease: "easeInOut",
+          duration: 2,
+          delayChildren: 0.5,
+          staggerChildren: 0.5,
+        }}
+      >
+        <motion.div
+          className="size-20 bg-cyan-500 rounded-md"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-500 rounded-md"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-500 rounded-md"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-500 rounded-md"
+          variants={child}
+        ></motion.div>
+      </motion.div>
     </div>
   );
 };
